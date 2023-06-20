50c0be3accc61fc874000523ecd13b367bf3c0e8
David Gomez <gomezreina.david@gmail.com>
2023-06-20 12:01:01 +0200

    category and subcategory views & urls

--- web/urls.py
+++ web/urls.py
@@ -4,5 +4,7 @@ from web import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    
+    path('productos/', views.products, name='products'),
+    path('productos/<str:category_slug>', views.category, name='category'),
+    path('productos/<str:category_slug>/<str:subcategory_slug>', views.category, name='subcategory'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,5 +1,8 @@
+from django.db.models import Q
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+
+from shop.models import Category, Product
 
 
 # Create your views here.
@@ -11,4 +14,33 @@ def index(request):
         return HttpResponseRedirect('/gracias/')
     else:
         return render(request, 'index.html', context)
+
+
+def products(request):
+    categories = Category.objects.all()
+
+    context = {
+        "ShopTitle": "Tienda",
+        "categories": categories,
+    }
+    return render(request, 'shop.html', context)
+
+
+def category(request, *args, **kwargs):
+    categories = Category.objects.all()
+    if kwargs.get('subcategory_slug'):
+        category_slug = kwargs.get('subcategory_slug')
+    else:
+        category_slug = kwargs.get('category_slug')
+
+    current_category = Category.objects.get(slug=category_slug)
+    products = Product.objects.filter(category=current_category)
+    context = {
+        "ShopTitle": current_category,
+        "categories": categories,
+
+        "category": current_category,
+        "products": products
+    }
+    return render(request, 'shop.html', context)
 
